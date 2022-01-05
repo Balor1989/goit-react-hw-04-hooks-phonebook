297e91df7cccf6f1c3ffddbbcc7d34c01a9ade4f
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-05 14:18:06 +0200

    adds refactoring App without localeStorage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import s from './App.module.css'
+import { useState} from 'react';
+import Contacts from "./Contacts/Contacts";
+import Phonebook from "./Phonebook/Phonebook";
+import shortid from "shortid";
+import Filter from "./Filter";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import contactEl from './Contacts/contacts.json'
 
-function App() {
+
+
+const App = () => {
+
+  const [contacts, setContacts] = useState(contactEl);
+  const [filter, setFilter] = useState('');
+
+  const filterChange = (e) => {
+    setFilter(e.target.value)
+  };
+
+  const addContactCard = (values) => {
+    const repeatName = contacts.find(contact => {
+      return contact.name.toLowerCase() === values.name.toLowerCase()
+    })
+    if (repeatName) {
+      Notify.warning(`${values.name} is already in contacts`)
+      return
+    }
+    const contact = {
+      id: shortid.generate(),
+      name: values.name,
+      number: values.number
+    }
+    setContacts(prev => {return [contact, ...prev] })
+    values.name = ''
+    values.number = ''
+    Notify.success(`${values.name} is added in contacts`)
+  };
+
+  const deleteContactCard = (cardId) => {
+    setContacts(prev => prev.filter((contact => contact.id !== cardId)))
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContactCards = contacts.filter( contact => contact.name.toLowerCase().includes(normalizedFilter))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className={s.phonebookSection}>
+      <Phonebook
+        onAddContactCard={addContactCard} />
+      <Filter onFilterChange={filterChange}
+        value={filter}
+      />
+      <Contacts contacts={visibleContactCards}
+        onDeleteContactCard={deleteContactCard}
+      />
+    </section>
   );
 }
-
 export default App;
+
+
+
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const { contacts } = this.state;
+//     if (contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(contacts))
+//     }
+//   };
+  
+//   componentDidMount() {
+//     const contacts = localStorage.getItem('contacts')
+//     const parseContacts = JSON.parse(contacts)
+//     if (!contacts) {
+//       return
+//     }
+//     this.setState({ contacts: parseContacts })
+//   };
+
+
+
+
+
+
+  
+
+
+  
+
+
+
---
+++ src/Phonebook/Phonebook.js
@@ -0,0 +1,70 @@
+import { Component } from "react/cjs/react.production.min";
+import { BsFillTelephonePlusFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
+import s from './Phonebook.module.css'
+
+
+class Phonebook extends Component {
+
+    state = {
+        name: '',
+        number:''
+    }
+
+    formSubmit = e => {
+        e.preventDefault()
+        this.props.onAddContactCard(this.state)
+        // this.setState({ number: '', name: '' })
+        
+    };
+
+    inputName = e => {
+        this.setState({ name: e.target.value })
+        console.log(e.target.value)
+    };
+
+    inputNumber = e => {
+        this.setState({ number: e.target.value })
+    };
+    
+    render() {
+        const{name, number} = this.state
+        return (
+                <>
+                <h2 className={s.phonebookTitle}>Phonebook 
+                    <IconContext.Provider value={{ color: "rgb(198, 131, 204)", size: "25px", style: { margin: "0 0 0 15px" } }}>
+                    <BsFillTelephonePlusFill />
+                    </IconContext.Provider></h2>
+                    <form className={s.phonebookForm} onSubmit={this.formSubmit} >
+                        <div className={s.inputBox}>
+                            <label className={s.label}>Name</label>
+                            <input className={s.input} value={name} onChange={this.inputName}
+                            type="text"
+                            name="name"
+                            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+                            required
+                            />
+                        </div>
+                        <div className={s.inputBox}>
+                            <label className={s.label}>Number</label>
+                            <input className={s.input} value={number} onChange={this.inputNumber}
+                            type="tel"
+                            name="number"
+                            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+                            required
+                            />
+                        </div>
+                        <div className={s.buttonBox}>
+                        <button className={s.deleteButton} type="submit">Add contact</button>
+                        </div>
+                    </form>
+                </>
+        )
+    }
+}
+
+
+
+export default Phonebook
